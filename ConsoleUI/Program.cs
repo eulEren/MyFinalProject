59bb1d0790dc82b02777fc2172f98d6d43af932d
Eren Ece <erenece595@gmail.com>
2026-06-29 10:27:05 +0300

    29 haziran

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,22 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleUI
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+            foreach (var product in productManager.GetByUnitPrice(40,100))
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+    }
+}
